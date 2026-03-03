d2242777b08f986284e87015c10944098230d86c
Jack Ball <ball.jack2376@gmail.com>
2026-03-03 13:21:16 -0700

    Fix - variable port

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -5,8 +5,15 @@ const Login = () => {
     <div className="login-page-wrapper">
       <div className="login-copy-wrapper">
         <img src="/p1p-logo-white.png" className="login-logo" alt="" />
-        <h2>Commission Portal</h2>
-        <LoginButton />
+        <div className="login-copy-container">
+          <h1>Commission Portal</h1>
+          <p>Login with your Page One Power Google account to continue</p>
+          <LoginButton />
+          <p className="login-subtext">
+            First time logging in? No need to create an account. Simply log in
+            with your Page One Power Google account
+          </p>
+        </div>
       </div>
     </div>
   );
