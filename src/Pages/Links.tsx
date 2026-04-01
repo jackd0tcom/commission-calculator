f5a35fdac09d032c2789ce885bd910646769d194
Jack Ball <ball.jack2376@gmail.com>
2026-04-01 11:56:03 -0600

    Add - links page

--- src/Pages/Links.tsx
+++ src/Pages/Links.tsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { useSelector } from "react-redux";
 import Loader from "../components/UI/Loader";
+import LinkItem from "../components/Links/LinkItem";
 
 const Links = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [linkList, setLinkList] = useState([{}]);
+  const user = useSelector((state: any) => state.user);
 
   const fetchLinks = async () => {
     try {
@@ -24,6 +27,35 @@ const Links = () => {
     fetchLinks();
   }, []);
 
+  const handleAddLink = async () => {
+    try {
+      await axios.post("/api/newLink").then((res) => {
+        if (res.status === 200) {
+          setLinkList((prev) => [res.data, ...prev]);
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDeleteLink = async (linkId: number) => {
+    try {
+      await axios.post("/api/deleteProduct", { linkId }).then((res) => {
+        if (res.status === 200) {
+          setLinkList((prev) =>
+            prev.filter(
+              (l) =>
+                Number((l as { linkId?: number }).linkId) !== Number(linkId),
+            ),
+          );
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="links-page-wrapper">
       <div className="page-header-wrapper">
@@ -36,10 +68,35 @@ const Links = () => {
           <div className="links-list-wrapper">
             <div className="links-list-item links-list-head">
               <p>#</p>
-              <p>Provider</p>
-              <p>URL</p>
               <p>Name</p>
-              <p></p>
+              <p>URL</p>
+              <p>Cost</p>
+              <p>Price</p>
+              <p>Commission Rate</p>
+              <p>Spiff</p>
+            </div>
+            <div className="links-list">
+              {linkList?.length > 0 &&
+                linkList?.map((link: any, index: number) => (
+                  <LinkItem
+                    link={link}
+                    index={index}
+                    isAdmin={user.isAdmin}
+                    key={(link as { linkId?: number }).linkId ?? index}
+                    handleDeleteLink={handleDeleteLink}
+                    linkList={linkList}
+                    setLinkList={setLinkList}
+                  />
+                ))}
+              {user?.isAdmin && (
+                <div
+                  onClick={() => handleAddLink()}
+                  className="product-list-item add-product-wrapper"
+                >
+                  <p>+</p>
+                  <p>Add Link</p>
+                </div>
+              )}
             </div>
           </div>
         </div>
---
+++ src/components/Links/LinkItem.tsx
@@ -0,0 +1,247 @@
+import { useState, useRef } from "react";
+import axios from "axios";
+import { TiDelete } from "react-icons/ti";
+
+interface props {
+  link: any;
+  index: number;
+  isAdmin: boolean;
+  handleDeleteLink: any;
+  linkList: any;
+  setLinkList: any;
+}
+
+const LinkItem = ({
+  link,
+  index,
+  isAdmin,
+  handleDeleteLink,
+  linkList,
+  setLinkList,
+}: props) => {
+  const [name, setName] = useState(link?.linkName ?? "Add a name");
+  const [cost, setCost] = useState(link?.cost ?? "");
+  const [url, setUrl] = useState(link?.url ?? "");
+  const [defaultPrice, setDefaultPrice] = useState(link?.defaultPrice ?? 0);
+  const [commissionRate, setCommissionRate] = useState(
+    link?.commissionRate ?? 0,
+  );
+  const [spiff, setSpiff] = useState(link?.spiff ?? 0.0);
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const updateLink = async (fieldName: string, value: string) => {
+    try {
+      await axios
+        .post("/api/updateLink", {
+          linkId: link.linkId,
+          fieldName,
+          value,
+        })
+        .then((res) => {
+          console.log(res.data);
+          if (res.status === 200) {
+            const listCopy = [...linkList];
+            const currentLink = listCopy.find(
+              (link: any) => link.linkId === link.linkId,
+            );
+            if (fieldName === "commissionRate") {
+              currentLink[fieldName] = value;
+            }
+            setLinkList(listCopy);
+            return;
+          }
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return isDeleting ? (
+    <div className="product-delete-modal">
+      <p>Are you sure you want to delete {name}?</p>
+      <div>
+        <button
+          className="delete-product"
+          onClick={() => {
+            handleDeleteLink(link.linkId);
+            setIsDeleting(false);
+          }}
+        >
+          Delete
+        </button>
+        <button
+          className="cancel-delete-product"
+          onClick={() => setIsDeleting(false)}
+        >
+          Cancel
+        </button>
+      </div>
+    </div>
+  ) : isAdmin ? (
+    <div className="links-list-item">
+      <p>{index + 1}</p>
+      <input
+        type="text"
+        className="product-list-name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        onBlur={() => {
+          if (name !== link.productName) {
+            updateLink("linkName", name);
+          }
+        }}
+        onKeyDown={(e) => {
+          if (name === link.productName) {
+            return;
+          }
+          if (e.key === "Enter") {
+            updateLink("linkName", name);
+          }
+        }}
+      />
+      <input
+        type="text"
+        className="product-list-name"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+        onBlur={() => {
+          if (url !== link.url) {
+            updateLink("url", url);
+          }
+        }}
+        onKeyDown={(e) => {
+          if (url === link.url) {
+            return;
+          }
+          if (e.key === "Enter") {
+            updateLink("url", url);
+          }
+        }}
+      />
+      <div className="link-list-number-wrapper">
+        <p>$</p>
+        <input
+          type="number"
+          className="link-list-number"
+          value={cost}
+          onChange={(e) => {
+            const val = Number(e.target.value);
+            if (val < 0) {
+              return;
+            }
+            setCost(e.target.value);
+          }}
+          onBlur={() => {
+            if (cost !== link.cost) {
+              updateLink("cost", cost);
+            }
+          }}
+          onKeyDown={(e) => {
+            if (cost === link.cost) {
+              return;
+            }
+            if (e.key === "Enter") {
+              updateLink("cost", cost);
+            }
+          }}
+        />
+      </div>
+      <div className="link-list-number-wrapper">
+        <p>$</p>
+        <input
+          type="number"
+          className="link-list-number"
+          value={defaultPrice}
+          onChange={(e) => {
+            const val = Number(e.target.value);
+            if (val < 0) {
+              return;
+            }
+            setDefaultPrice(e.target.value);
+          }}
+          onBlur={() => {
+            if (defaultPrice !== link.defaultPrice) {
+              updateLink("defaultPrice", defaultPrice);
+            }
+          }}
+          onKeyDown={(e) => {
+            if (defaultPrice === link.defaultPrice) {
+              return;
+            }
+            if (e.key === "Enter") {
+              updateLink("defaultPrice", defaultPrice);
+            }
+          }}
+        />
+      </div>
+      <input
+        type="number"
+        className="link-list-number"
+        value={commissionRate}
+        onChange={(e) => {
+          const val = Number(e.target.value);
+          if (val < 0) {
+            return;
+          }
+          setCommissionRate(e.target.value);
+        }}
+        onBlur={() => {
+          if (commissionRate !== link.commissionRate) {
+            updateLink("commissionRate", commissionRate);
+          }
+        }}
+        onKeyDown={(e) => {
+          if (commissionRate === link.commissionRate) {
+            return;
+          }
+          if (e.key === "Enter") {
+            updateLink("commissionRate", commissionRate);
+          }
+        }}
+      />
+      <div className="link-list-number-wrapper">
+        <p>$</p>
+        <input
+          type="number"
+          className="link-list-number"
+          value={spiff}
+          onChange={(e) => {
+            const val = Number(e.target.value);
+            if (val < 0) {
+              return;
+            }
+            setSpiff(e.target.value);
+          }}
+          onBlur={() => {
+            if (spiff !== link.spiff) {
+              updateLink("spiff", spiff);
+            }
+          }}
+          onKeyDown={(e) => {
+            if (spiff === link.spiff) {
+              return;
+            }
+            if (e.key === "Enter") {
+              updateLink("spiff", spiff);
+            }
+          }}
+        />
+      </div>
+      <TiDelete
+        className="sheet-item-delete"
+        onClick={() => setIsDeleting(true)}
+      />
+    </div>
+  ) : (
+    <div className="links-list-item">
+      <p>{index + 1}</p>
+      <p>{name}</p>
+      <a href={url}>{url}</a>
+      <p>{cost}</p>
+      <p>{defaultPrice}</p>
+      <p>{commissionRate}</p>
+      <p>{spiff}</p>
+    </div>
+  );
+};
+export default LinkItem;
