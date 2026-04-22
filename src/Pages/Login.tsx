d2715abba70db2b736b1e8d9ade7135ea979f49f
Jack Ball <ball.jack2376@gmail.com>
2026-04-22 17:57:29 -0600

    Fix - p1p portal

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -6,7 +6,7 @@ const Login = () => {
       <div className="login-copy-wrapper">
         <img src="/p1p-logo-white.png" className="login-logo" alt="" />
         <div className="login-copy-container">
-          <h1>Commission Portal</h1>
+          <h1>P1P Portal</h1>
           <p>Login with your Page One Power Google account to continue</p>
           <LoginButton />
           <p className="login-subtext">
