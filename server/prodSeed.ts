d63d123a94590ff4e9eb871e4b340c95998bd1d5
Jack Ball <ball.jack2376@gmail.com>
2026-05-01 14:52:48 -0600

    Add - fixed prodSeed

--- server/prodSeed.ts
+++ server/prodSeed.ts
@@ -358,7 +358,7 @@ const linkSeedData = [
 ];
 
 async function seed() {
-  await db.sync({ alter: true });
+  await db.sync({ force: true });
 
   const products = await Product.bulkCreate(productSeedData, {
     ignoreDuplicates: true,
