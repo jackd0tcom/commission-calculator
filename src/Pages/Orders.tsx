2ca5de8b2f2e0513097b1e215c2a1b9bd406c6c7
Jack Ball <ball.jack2376@gmail.com>
2026-06-15 15:08:05 -0600

    Add - orders sorting / searching

--- src/Pages/Orders.tsx
+++ src/Pages/Orders.tsx
@@ -6,6 +6,7 @@ import { formatDateNoTime } from "../helpers";
 import OrderStatusBadge from "../components/Orders/OrderStatusBadge";
 import Loader from "../components/UI/Loader";
 import OrderFilterDropdown from "../components/Orders/OrderFilterDropdown";
+import OrderSort from "../components/Orders/OrderSort";
 
 type FilterOption = {
   title: string;
@@ -21,10 +22,13 @@ const Orders = () => {
     user: 0,
     client: [],
     status: [],
+    sort: "",
+    direction: "up",
   });
   const [salesPeople, setSalesPeople] = useState<FilterOption[]>([]);
   const [clients, setClients] = useState<FilterOption[]>([]);
   const [statuses, setStatuses] = useState<FilterOption[]>([]);
+  const [search, setSearch] = useState("");
 
   const fetchOrders = async () => {
     try {
@@ -82,67 +86,138 @@ const Orders = () => {
   }, []);
 
   // Order filter handler
-  const { submittedOrders, progressOrders, partialOrders, deliveredOrders } =
-    useMemo(() => {
-      const submittedOrders: any = [];
-      const progressOrders: any = [];
-      const partialOrders: any = [];
-      let deliveredOrders: any = [];
+  const {
+    allOrders,
+    submittedOrders,
+    progressOrders,
+    partialOrders,
+    deliveredOrders,
+  } = useMemo(() => {
+    const submittedOrders: any = [];
+    const progressOrders: any = [];
+    const partialOrders: any = [];
+    let deliveredOrders: any = [];
 
-      let data: any = orders;
-      // console.log(filter);
+    let data: any;
 
+    // Search
+    if (search.trim() === "") {
+      data = orders;
+    } else {
+      data = orders;
+      const searchTerm = search.toLowerCase();
       data = data.filter((order: any) => {
-        if (!order) {
+        const orderTitle = `order #${order.orderId}`;
+        const salesPersonName = `${order.user.firstName} ${order.user.lastName}`;
+        const date = new Date(order.createdAt).toDateString();
+        const numericDate = new Date(order.createdAt).toLocaleDateString();
+
+        // Return true since searchTerm only has to match with one of the items, not match all criteria
+
+        // Search in clients
+        if (order.client?.clientName?.toLowerCase().includes(searchTerm))
+          return true;
+
+        // Search in order title?
+        if (orderTitle.includes(searchTerm)) return true;
+
+        // Search in salesperson
+        if (salesPersonName?.toLowerCase().includes(searchTerm)) return true;
+
+        // Search date
+        if (date?.toLowerCase().includes(searchTerm)) return true;
+
+        // Search numeric date
+        if (numericDate?.toLowerCase().includes(searchTerm)) return true;
+
+        // Search status date
+        if (order.orderStatus?.toLowerCase().includes(searchTerm)) return true;
+      });
+    }
+
+    // Filters
+    data = data.filter((order: any) => {
+      if (!order) {
+        return false;
+      }
+      if (filter.client.length > 0)
+        if (!filter.client.some((client: any) => client.id === order.clientId))
           return false;
+
+      if (filter.user !== 0)
+        if (order.salesPerson !== filter.user) return false;
+
+      if (filter.status.length > 0)
+        if (
+          !filter.status.some(
+            (status: any) => status.title === order.orderStatus,
+          )
+        )
+          return false;
+
+      return true;
+    });
+
+    // Sorting
+    data = data.sort((a: any, b: any) => {
+      const statusOrder = ["in progress", "partial", "delivered"];
+      switch (filter.sort) {
+        case "dateCreated":
+          return filter.direction === "up"
+            ? new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+            : new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+
+        case "client":
+          return filter.direction === "up"
+            ? a.client?.clientName.localeCompare(b.client?.clientName)
+            : b.client?.clientName.localeCompare(a.client?.clientName);
+
+        case "status":
+          return filter.direction === "up"
+            ? statusOrder.indexOf(a.orderStatus) -
+                statusOrder.indexOf(b.orderStatus)
+            : statusOrder.indexOf(b.orderStatus) -
+                statusOrder.indexOf(a.orderStatus);
+
+        case "number":
+          return filter.direction === "up"
+            ? a.orderId - b.orderId
+            : b.orderId - a.orderId;
+
+        default:
+          break;
+      }
+    });
+
+    data.forEach((order: any) => {
+      if (order.order_items?.length > 0) {
+        if (order.orderStatus === "submitted") {
+          submittedOrders.push(order);
+          return;
         }
-        if (filter.client.length > 0)
-          if (
-            !filter.client.some((client: any) => client.id === order.clientId)
-          )
-            return false;
-
-        if (filter.user !== 0)
-          if (order.salesPerson !== filter.user) return false;
-
-        if (filter.status.length > 0)
-          if (
-            !filter.status.some(
-              (status: any) => status.title === order.orderStatus,
-            )
-          )
-            return false;
-
-        return true;
-      });
-
-      data.forEach((order: any) => {
-        if (order.order_items?.length > 0) {
-          if (order.orderStatus === "submitted") {
-            submittedOrders.push(order);
-            return;
-          }
-          const undeliveredItems = order.order_items.filter(
-            (item: any) => item.itemStatus !== "complete",
+        const undeliveredItems = order.order_items.filter(
+          (item: any) => item.itemStatus !== "complete",
+        );
+        if (undeliveredItems.length > 0) {
+          const deliveredItems = order.order_items.filter(
+            (item: any) => item.itemStatus === "complete",
           );
-          if (undeliveredItems.length > 0) {
-            const deliveredItems = order.order_items.filter(
-              (item: any) => item.itemStatus === "complete",
-            );
-            if (deliveredItems.length > 0) {
-              partialOrders.push(order);
-            } else progressOrders.push(order);
-          } else deliveredOrders.push(order);
-        } else progressOrders.push(order);
-      });
+          if (deliveredItems.length > 0) {
+            partialOrders.push(order);
+          } else progressOrders.push(order);
+        } else deliveredOrders.push(order);
+      } else progressOrders.push(order);
+    });
 
-      return {
-        submittedOrders,
-        progressOrders,
-        partialOrders,
-        deliveredOrders,
-      };
-    }, [orders, filter]);
+    const allOrders = data;
+    return {
+      allOrders,
+      submittedOrders,
+      progressOrders,
+      partialOrders,
+      deliveredOrders,
+    };
+  }, [orders, filter, search]);
 
   return (
     <div className="commissions-page-wrapper">
@@ -158,6 +233,17 @@ const Orders = () => {
         </button>
       </div>
       <div className="orders-page-body">
+        <div className="orders-filter-box-wrapper">
+          <OrderSort filter={filter} setFilter={setFilter} />
+          <div className="orders-search-wrapper">
+            <input
+              type="text"
+              className="orders-search-input"
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="Search"
+            />
+          </div>
+        </div>
         <div className="orders-lists">
           {isLoading ? (
             <Loader />
@@ -193,174 +279,44 @@ const Orders = () => {
                   </div>
                 </div>
               </div>
-              {submittedOrders.length > 0 && (
-                <div className="orders-list-wrapper">
-                  <h3>Submitted Orders</h3>
-                  <div className="orders-list">
-                    {submittedOrders?.length > 0 ? (
-                      submittedOrders.map((order: any) => {
-                        const deliveredItems = order.order_items.filter(
-                          (item: any) => item.itemStatus === "complete",
-                        );
-                        return (
-                          <div
-                            className="orders-list-item"
-                            key={`order-${order.orderId}`}
-                            onClick={() =>
-                              navigate(`/order/${order.orderId}/false`)
-                            }
-                          >
-                            <div className="user-width">
-                              <ProfilePic src={order.user?.profilePic} />
-                            </div>
-                            <div>Order #{order.orderId}</div>
-                            <div>{order.client?.clientName}</div>
-                            <div>
-                              <OrderStatusBadge status={"submitted"} />
-                            </div>
-                            <div>
-                              {deliveredItems.length} /{" "}
-                              {order.order_items.length}
-                            </div>
-                            <div>{formatDateNoTime(order.createdAt)}</div>
+              <div className="orders-list-wrapper">
+                <div className="orders-list">
+                  {allOrders?.length > 0 ? (
+                    allOrders.map((order: any) => {
+                      const deliveredItems = order.order_items.filter(
+                        (item: any) => item.itemStatus === "complete",
+                      );
+                      return (
+                        <div
+                          className="orders-list-item"
+                          key={`order-${order.orderId}`}
+                          onClick={() =>
+                            navigate(`/order/${order.orderId}/false`)
+                          }
+                        >
+                          <div className="user-width">
+                            <ProfilePic src={order.user?.profilePic} />
                           </div>
-                        );
-                      })
-                    ) : (
-                      <div className="orders-list-item">
-                        <p></p>
-                        <p>No orders</p>
-                      </div>
-                    )}
-                  </div>
-                </div>
-              )}
-              {progressOrders.length > 0 && (
-                <div className="orders-list-wrapper">
-                  <h3>Orders In Progress</h3>
-                  <div className="orders-list">
-                    {progressOrders?.length > 0 ? (
-                      progressOrders.map((order: any) => {
-                        const deliveredItems = order.order_items?.filter(
-                          (item: any) => item.itemStatus === "complete",
-                        );
-                        return (
-                          <div
-                            className="orders-list-item"
-                            key={`order-${order.orderId}`}
-                            onClick={() =>
-                              navigate(`/order/${order.orderId}/false`)
-                            }
-                          >
-                            <div className="user-width">
-                              <ProfilePic src={order.user?.profilePic} />
-                            </div>
-                            <div>Order #{order.orderId}</div>
-                            <div>{order.client?.clientName}</div>
-                            <div>
-                              <OrderStatusBadge status={"in progress"} />
-                            </div>
-                            <div>
-                              {deliveredItems?.length} /{" "}
-                              {order.order_items?.length}
-                            </div>
-                            <div>{formatDateNoTime(order.createdAt)}</div>
+                          <div>Order #{order.orderId}</div>
+                          <div>{order.client?.clientName}</div>
+                          <div>
+                            <OrderStatusBadge status={order.orderStatus} />
                           </div>
-                        );
-                      })
-                    ) : (
-                      <div className="orders-list-item">
-                        <p></p>
-                        <p>No orders</p>
-                      </div>
-                    )}
-                  </div>
-                </div>
-              )}
-              {partialOrders.length > 0 && (
-                <div className="orders-list-wrapper">
-                  <h3>Partially Delivered Orders</h3>
-                  <div className="orders-list">
-                    {partialOrders?.length > 0 ? (
-                      partialOrders.map((order: any) => {
-                        const deliveredItems = order.order_items.filter(
-                          (item: any) => item.itemStatus === "complete",
-                        );
-                        return (
-                          <div
-                            className="orders-list-item"
-                            key={`order-${order.orderId}`}
-                            onClick={() =>
-                              navigate(`/order/${order.orderId}/false`)
-                            }
-                          >
-                            <div className="user-width">
-                              <ProfilePic src={order.user?.profilePic} />
-                            </div>
-                            <div>Order #{order.orderId}</div>
-                            <div>{order.client?.clientName}</div>
-                            <div>
-                              <OrderStatusBadge status={"partial"} />
-                            </div>
-                            <div>
-                              {deliveredItems.length} /{" "}
-                              {order.order_items.length}
-                            </div>
-                            <div>{formatDateNoTime(order.createdAt)}</div>
+                          <div>
+                            {deliveredItems.length} / {order.order_items.length}
                           </div>
-                        );
-                      })
-                    ) : (
-                      <div className="orders-list-item">
-                        <p></p>
-                        <p>No orders</p>
-                      </div>
-                    )}
-                  </div>
+                          <div>{formatDateNoTime(order.createdAt)}</div>
+                        </div>
+                      );
+                    })
+                  ) : (
+                    <div className="orders-list-item">
+                      <p></p>
+                      <p>No orders</p>
+                    </div>
+                  )}
                 </div>
-              )}
-              {deliveredOrders.length > 0 && (
-                <div className="orders-list-wrapper">
-                  <h3>Completed Orders</h3>
-                  <div className="orders-list">
-                    {deliveredOrders?.length > 0 ? (
-                      deliveredOrders.map((order: any) => {
-                        const deliveredItems = order.order_items.filter(
-                          (item: any) => item.itemStatus === "complete",
-                        );
-                        return (
-                          <div
-                            className="orders-list-item"
-                            key={`order-${order.orderId}`}
-                            onClick={() =>
-                              navigate(`/order/${order.orderId}/false`)
-                            }
-                          >
-                            <div className="user-width">
-                              <ProfilePic src={order.user?.profilePic} />
-                            </div>
-                            <div>Order #{order.orderId}</div>
-                            <div>{order.client?.clientName}</div>
-                            <div>
-                              <OrderStatusBadge status={"delivered"} />
-                            </div>
-                            <div>
-                              {deliveredItems.length} /{" "}
-                              {order.order_items.length}
-                            </div>
-                            <div>{formatDateNoTime(order.createdAt)}</div>
-                          </div>
-                        );
-                      })
-                    ) : (
-                      <div className="orders-list-item">
-                        <p></p>
-                        <p>No orders</p>
-                      </div>
-                    )}
-                  </div>
-                </div>
-              )}
+              </div>
             </>
           )}
         </div>
