03f579df616e2f3f907765491bcf473f17b8faa4
Jack Ball <ball.jack2376@gmail.com>
2026-06-29 14:01:43 -0600

    Add - filtering on commissions page

--- src/Pages/Commissions.tsx
+++ src/Pages/Commissions.tsx
@@ -5,13 +5,6 @@ const Commissions = () => {
     <div className="commissions-page-wrapper">
       <div className="page-header-wrapper">
         <h2>Commission Sheets</h2>
-        {/* <button
-          onClick={() => handleNewSheet()}
-          className="new-sheet-button"
-          disabled={sheetExists}
-        >
-          New Sheet
-        </button> */}
       </div>
       <CommissionSheetList />
     </div>
--- src/components/Commissions/CommissionSheetList.tsx
+++ src/components/Commissions/CommissionSheetList.tsx
@@ -1,19 +1,36 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import axios from "axios";
 import StatusBadge from "../UI/StatusBadge";
-import { useNavigate } from "react-router";
+import { useNavigate, useRouteLoaderData } from "react-router";
 import ProfilePic from "../UI/ProfilePic";
-import { formatRelativeTime } from "../../helpers";
+import { formatDateWithDay } from "../../helpers";
 import Loader from "../UI/Loader";
 import { useSelector } from "react-redux";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { formatDollar } from "../../helpers";
+import FilterDropdown from "../UI/FilterDropdown";
+
+type FilterOption = {
+  title: string;
+  id?: number;
+  profilePic?: string;
+};
 
 const CommissionSheetList = () => {
   const [commissionList, setCommissionList] = useState([{}]);
   const [isLoading, setIsLoading] = useState(true);
   const nav = useNavigate();
   const userId = useSelector((state: any) => state.user.userId);
+  const [filter, setFilter] = useState({
+    sort: "",
+    user: [],
+    title: [],
+    status: [],
+    direction: "up",
+  });
+  const [userList, setUserList] = useState<FilterOption[]>([]);
+  const [titles, setTitles] = useState<FilterOption[]>([]);
+  const [statuses, setStatuses] = useState<FilterOption[]>([]);
 
   const getCommissionSheets = async () => {
     try {
@@ -21,6 +38,42 @@ const CommissionSheetList = () => {
         if (res.status === 200) {
           setCommissionList(res.data);
           setIsLoading(false);
+
+          let users: FilterOption[] = [];
+          let titleArray: FilterOption[] = [];
+          let statusArray: FilterOption[] = [];
+
+          res.data.forEach((order: any) => {
+            if (!users.some((user: any) => order.user?.userId === user.id)) {
+              users.push({
+                title: `${order.user.firstName ?? ""}`,
+                id: order.userId,
+                profilePic: order.user.profilePic ?? "",
+              });
+            }
+            if (
+              !titleArray.some((title: any) => order.sheetTitle === title.title)
+            ) {
+              titleArray.push({
+                title: order.sheetTitle,
+                id: order.sheetTitle,
+              });
+            }
+            if (
+              !statusArray.some(
+                (status: any) => order.sheetStatus === status.id,
+              )
+            ) {
+              statusArray.push({
+                title: order.sheetStatus,
+                id: order.sheetStatus,
+              });
+            }
+          });
+
+          setUserList(users);
+          setTitles(titleArray);
+          setStatuses(statusArray);
         }
       });
     } catch (error) {
@@ -33,6 +86,33 @@ const CommissionSheetList = () => {
       getCommissionSheets();
     }
   }, [userId]);
+
+  const filteredList = useMemo(() => {
+    let data = commissionList;
+
+    // filter
+    if (filter.user.length > 0) {
+      data = data.filter((commission: any) =>
+        filter.user.some((user: any) => user.id === commission.userId),
+      );
+    }
+    if (filter.title.length > 0) {
+      data = data.filter((commission: any) =>
+        filter.title.some(
+          (title: any) => title.title === commission.sheetTitle,
+        ),
+      );
+    }
+    if (filter.status.length > 0) {
+      data = data.filter((commission: any) =>
+        filter.status.some(
+          (status: any) => status.title === commission.sheetStatus,
+        ),
+      );
+    }
+
+    return data;
+  }, [filter, commissionList]);
 
   const getTotal = (deliveries: any) => {
     return deliveries.reduce((acc: number, delivery: any) => {
@@ -55,30 +135,64 @@ const CommissionSheetList = () => {
       ) : (
         <div className="commission-sheet-list">
           <div className="commission-sheet-header">
-            <p>User</p>
-            <p>Title</p>
-            <p>Status</p>
-            <p>Commission</p>
-            <p>Date Created</p>
+            <div className="filter-dropdown-wrapper">
+              <FilterDropdown
+                heading="User"
+                array={true}
+                options={userList}
+                filter={filter}
+                setFilter={setFilter}
+              />
+            </div>
+            <div className="commission-sheet-header-item">
+              <FilterDropdown
+                heading="Title"
+                array={true}
+                options={titles}
+                filter={filter}
+                setFilter={setFilter}
+              />
+            </div>
+            <div className="commission-sheet-header-item">
+              {" "}
+              <FilterDropdown
+                heading="Status"
+                array={true}
+                options={statuses}
+                filter={filter}
+                setFilter={setFilter}
+              />
+            </div>
+            <div className="commission-sheet-header-item">Commission</div>
+            <div className="commission-sheet-header-item">Date Created</div>
           </div>
-          {commissionList?.length > 0 ? (
-            commissionList?.map((sheet: any) => (
+          {filteredList?.length > 0 ? (
+            filteredList?.map((sheet: any) => (
               <div
                 className="commission-sheet-item"
                 onClick={() => nav(`/sheet/${sheet.sheetId}`)}
               >
-                <ProfilePic src={sheet.user.profilePic} />
-                <p>{sheet.sheetTitle}</p>
-                <StatusBadge status={sheet.sheetStatus} />
-                <p>{formatDollar(getTotal(sheet.deliveries))}</p>
-                <p>{formatRelativeTime(sheet.createdAt)}</p>
+                <div className="commission-sheet-header-item commission-profile">
+                  <ProfilePic src={sheet.user.profilePic} />
+                </div>
+                <div className="commission-sheet-header-item">
+                  {sheet.sheetTitle}
+                </div>
+                <div className="commission-sheet-header-item">
+                  <StatusBadge status={sheet.sheetStatus} />
+                </div>
+                <div className="commission-sheet-header-item">
+                  {formatDollar(getTotal(sheet.deliveries))}
+                </div>
+                <div className="commission-sheet-header-item">
+                  {formatDateWithDay(sheet.createdAt)}
+                </div>
               </div>
             ))
           ) : (
             <div className="no-sheets">
               <FaMagnifyingGlass className="no-sheets-icon" />
               <h3>No sheets found</h3>
-              <p>Hit the button in the top right to create a new sheet</p>
             </div>
           )}
         </div>
