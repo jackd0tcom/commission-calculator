27776587761a1735300c251671aa5ecfb136df81
Jack Ball <ball.jack2376@gmail.com>
2026-07-06 10:39:18 -0600

    switching to dev

---
+++ src/Pages/Production.tsx
@@ -0,0 +1,11 @@
+const Production = () => {
+  return (
+    <div className="production-page-wrapper">
+      <div className="page-header-wrapper order-page-header">
+        <h2>Production</h2>
+      </div>
+      <div className="production-page-body"></div>
+    </div>
+  );
+};
+export default Production;
